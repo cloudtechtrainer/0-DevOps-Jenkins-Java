b8deea7005065181b0d3337e98e9377fdd00eea1
cloudtechtrainer <126149128+cloudtechtrainer@users.noreply.github.com>
2023-04-04 07:54:52 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-public class hellow {
+public class hello {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Hello Worlds " + i);
